aa00b2c6c37d11e39014dcee496c4726da871767
AndrewLutsky <andrew.lutsky19@gmail.com>
2023-10-20 09:53:14 -0400

    added get abs and cl argument functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 #INPUTS: A string query
 #OUTPUTS: A json internal file that contains the text from the pubmed scripts
-def GetABS(query, resultNum):
+def GetABS(db, query, resultNum):
     print(f"Lookup query is {query}")
     print(f"Looking for result number {resultNum}")
     
@@ -10,7 +10,7 @@ def GetABS(query, resultNum):
  
     # If your shell script has shebang, 
     # you can omit shell=True argument.
-    subprocess.run(["./ed.sh", f"pubmed {query} {resultNum}"], shell=True)
+    subprocess.run(["./ed.sh " + f"\"{db}\" \"{query}\" {resultNum}"], shell=True)
 
 
 
@@ -18,6 +18,7 @@ def GetABS(query, resultNum):
 
 if __name__ == '__main__':
     print(f"Searching pubmed for query : {sys.argv[1]}")
-    queryString = sys.argv[1]
-
+    db = sys.argv[1]
+    queryString = sys.argv[2]
+    GetABS(db, queryString, 500)
 
