cdb6481c01a0585c88a3483d79ba0da4ef07cccc
AndrewLutsky <andrew.lutsky19@gmail.com>
2023-10-19 22:25:19 -0400

    added main cli

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+#INPUTS: A string query
+#OUTPUTS: A json internal file that contains the text from the pubmed scripts
+def GetJSON(query, resultNum):
+    print(f"Lookup query is {query}")
+    print(f"Looking for result number {resultNum}")
+
+
+
+
+
+if __name__ == '__main__':
+    print(f"Searching pubmed for query : {sys.argv[1]}")
+    queryString = sys.argv[1]
+
+
