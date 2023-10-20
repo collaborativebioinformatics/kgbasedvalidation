83f8f03af687571aee17484a09db36156716a1ee
AndrewLutsky <andrew.lutsky19@gmail.com>
2023-10-20 09:42:21 -0400

    edited get abstract pubmed articles

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sys
-
+import subprocess
 #INPUTS: A string query
 #OUTPUTS: A json internal file that contains the text from the pubmed scripts
-def GetJSON(query, resultNum):
+def GetABS(query, resultNum):
     print(f"Lookup query is {query}")
     print(f"Looking for result number {resultNum}")
+    
+ 
+ 
+    # If your shell script has shebang, 
+    # you can omit shell=True argument.
+    subprocess.run(["./ed.sh", f"pubmed {query} {resultNum}"], shell=True)
 
 
 
